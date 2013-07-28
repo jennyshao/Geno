e087a734a2d63931dacf38010c2f9676889132f9
Juan Shao <jshao111@gmail.com>
2013-07-27 23:42:41 -0500

    Update gen_master_input_list.py
    what is the goal?

--- src/gen_master_input_list.py
+++ src/gen_master_input_list.py
@@ -25,11 +25,16 @@ for i in NaNs_freq:
 		comb_dir_localname = "NaN_%s_MAF_%s" % (str(i),str(j))
 		dirname=os.path.join(b.paramFiles_path,comb_dir_localname)
 		print "this is dirname: "+dirname
+		prepGmatrix_localname="G_G_NaNs_%s_prop.MAF_%s.rda" % (str(i),str(j))
 		for k in reps:
 			parameters_filename="parameters_%s_%s_%03d.R" % (str(i),str(j),k)
 			parameters_filename1=dirname+"/"+parameters_filename
 	#		print parameters_filename
-			os.mknod(parameters_filename1)
+			os.mknod(parameters_filename1)	
+			prepData_localname="Y_random.r_%s.csv" % (str(k))
+			print "%s_%s %s %s %s" % (combination_dir_localname,rep_dir,parameters_filename, \
+                                                prepData_localname,prepGmatrix_localname)
+
 #print comb_dir_fullname
 #print b.parameters_filename
 #for key in comb_dir_fullname:
